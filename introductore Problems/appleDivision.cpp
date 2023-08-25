c91534bb607d2162e47881a25ce0de636affe921
pedrinhoeu <pelusimoes3@gmail.com>
2023-08-24 21:02:21 -0300

    .

--- introductore Problems/appleDivision.cpp
+++ introductore Problems/appleDivision.cpp
@@ -1,8 +1,9 @@
 #include<bits/stdc++.h>
-
 using namespace std;
+typedef long long ll;
 
 int main() {
+    cin.tie(0)->sync_with_stdio(0);
     int n;
     cin >> n;
     
@@ -11,34 +12,25 @@ int main() {
         cin >> macas[i];
     }
     
-    int minresp = 0;
-    int resp1 = 0;
-    int resp2 = 0;
+    ll minresp = 1e18;
     
-    for (int i = 0; i <= pow(2, n)/2; i++) {
-        string z = bitset<32>(i).to_string();
-        z = z.substr(32 - n);
+
+
+    for (int i = 0; i < 1<<n; i++) {
+        ll temp1 = 0, temp2 = 0;
         
-        int tim1 = 0;
-        int tim2 = 0;
-        int j = 0;
-        
-        while (j < z.length()) {
-            if (z[j] == '0') {
-                tim1 += macas[j];
-            } else {
-                tim2 += macas[j];
+        for (int j = 0; j < n; j++){
+            if((1<<j) & i){
+                temp1 += macas[j];
             }
-            j++;
+            else{
+                temp2 += macas[j];
+            }
         }
-        
-        if (abs(tim1 - tim2) < abs(resp1 - resp2) || resp1 == 0 || resp2 == 0) {
-            resp1 = tim1;
-            resp2 = tim2;
-        }
+        minresp = min(minresp, abs(temp1 - temp2));
     }
+    cout << minresp << "\n";
     
-    cout << abs(resp1 - resp2) << "\n";
     
     return 0;
 }
--- introductore Problems/towerOfHanoi.cpp
+++ introductore Problems/towerOfHanoi.cpp
@@ -2,10 +2,25 @@
 using namespace std;
 typedef long long ll;
 
-void build(ll n, ll ori, ll fim, ll fal){
+ll som=0;
 
-}
+vector<pair<ll, ll>> resp;
+
+void build(ll n, ll ori, ll fim, ll fal){
+    if(n == 0) return;
+    som ++;
+    build(n-1, ori, fal, fim);
+    resp.push_back({ori, fal});
+    build(n-1, fim, ori, fal);
+} 
 
 int main(){
     cin.tie(0)->sync_with_stdio(0);
+    ll n;
+    cin >> n;
+    build(n, 1, 2, 3);
+    cout << som << "\n";
+    for(auto i : resp){
+        cout << i.first << " " << i.second << "\n";
+    }
 }
