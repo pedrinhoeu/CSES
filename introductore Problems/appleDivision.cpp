9d26dc2f292f5045a20aa7321809daf630d7bd04
pedrinhoeu <pelusimoes3@gmail.com>
2023-08-24 21:39:55 -0300

    .

--- introductore Problems/appleDivision.cpp
+++ introductore Problems/appleDivision.cpp
@@ -6,15 +6,11 @@ int main() {
     cin.tie(0)->sync_with_stdio(0);
     int n;
     cin >> n;
-    
     vector<int> macas(n);
     for (int i = 0; i < n; ++i) {
         cin >> macas[i];
     }
-    
     ll minresp = 1e18;
-    
-
 
     for (int i = 0; i < 1<<n; i++) {
         ll temp1 = 0, temp2 = 0;
@@ -29,8 +25,6 @@ int main() {
         }
         minresp = min(minresp, abs(temp1 - temp2));
     }
-    cout << minresp << "\n";
-    
-    
+    cout << minresp << "\n";    
     return 0;
 }
